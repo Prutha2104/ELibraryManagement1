d4091fcd4717594dca8db87c0dcf548ce03f9e99
Prutha Desai <desaipruth0105@gmail.com>
2021-04-05 23:51:51 +0530

    added page link to asp button

--- WebApplication1/Site1.Master.cs
+++ WebApplication1/Site1.Master.cs
@@ -13,5 +13,47 @@ namespace WebApplication1
         {
 
         }
+        protected void LinkButton6_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("AdminLogin.aspx");
+    }   
+        
+
+    protected void LinkButton11_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("adminauthormanagement.aspx");
+    }
+
+    protected void LinkButton12_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("adminpublishermanagement.aspx");
+    }
+
+    protected void LinkButton8_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("BookInventoryPage.aspx");
+    }
+
+    protected void LinkButton9_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("adminbookissuing.aspx");
+    }
+
+    protected void LinkButton10_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("AdminManagement.aspx");
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("UserProfile.aspx");
+
+    }
+
+    protected void LinkButton2_Click(object sender, EventArgs e)
+    {
+            Response.Redirect("UserSignUp.aspx");
+    }
+
     }
 }
